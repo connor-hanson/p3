4ae5c857c6404f2c4df09737a8512d48458d14ca
reatreify <70864815+reatreify@users.noreply.github.com>
2020-10-31 19:36:21 -0500

    Update main.c

--- main.c
+++ main.c
@@ -2,15 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include "parser.h"
-#include "graph.h"
 
 int main(int argc, char* argv[]) {
     FILE *make = getMakeFile(); // file
-    int graphMade = initGraph();
+    /*int graphMade = initGraph();
     if (graphMade == 0) {
         printf("error creatig graph");
         exit(0);
-    }
+    }*/
 
     // if no additional arg, first build spec found in file
     if (argc == 1) {
