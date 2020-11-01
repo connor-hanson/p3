dda59544a76f1bef4674012c78784625a6bb2817
reatreify <70864815+reatreify@users.noreply.github.com>
2020-10-31 19:35:24 -0500

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,24 +1,30 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-typedef struct {
-	char *target; // same as node name
-	char *dependencies;
-	//char *commands;
-	char **commands;
-	int numCommands;
-	int commandCap;
-} GraphNode;
+typedef struct GraphNode GraphNode;
 
-int initGraph();
-GraphNode *addNode(char* tar);
-int removeNode(char* tar);
+struct GraphNode{
+	char *name; // same as node name
+	GraphNode **dependencies;
+	char **commands;
+	int depSize;
+	int numDep;
+	int cmdSize;
+	int numCmd;
+	int hasBeenVisited;
+};
+
+//int initGraph();
+GraphNode *addNode(char* name);
+//int removeNode(char* name);
 int addNodeDep(GraphNode *node, char *dep);
 int addNodeCmd(GraphNode *node, char *cmd);
-int executeNodes(char* tar);
+void executeNodes(GraphNode *root, char *visitedNodes);
+GraphNode *findNode(GraphNode *startNode, char *name);
 char **getDependencies(char* dep); // what does this do again?
-
+void freeNode(GraphNode *root);
 int getNumNodes();
+int childModded(GraphNode *parent, GraphNode *child);
 GraphNode* getGraphRoot();
 
 #endif
