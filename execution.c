43602501ede95c8d1b314e34453e15ceb9c3a483
reatreify <70864815+reatreify@users.noreply.github.com>
2020-11-02 14:54:31 -0600

    Update execution.c

--- execution.c
+++ execution.c
@@ -2,53 +2,77 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "execution.h"
 
 void executeLine(char *line) {
-	int lineLength = (int)strlen(line);
+	pid_t pid;
+  	int status;
+  	pid = fork();
+  	int lineLength = (int)strlen(line)+1;
 	char *tempLine1 = malloc(lineLength * sizeof(char));
-	if (tempLine1 != NULL) {
-		char *tempLine2 = malloc(lineLength * sizeof(char));
-		if (tempLine2 != NULL) {
-			strcpy(tempLine1, line);
-			strcpy(tempLine2, line);
-			char *separator = " ";
-			char *arg = strtok(tempLine1, separator);
-			arg = strtok(NULL, separator);
-			int argNum = 0;
-			while (arg != NULL) {
-				argNum++;
+	char *tempLine2 = malloc(lineLength * sizeof(char));
+	if (pid == 0) {
+		if (tempLine1 != NULL) {
+			if (tempLine2 != NULL) {
+				strcpy(tempLine1, line);
+				strcpy(tempLine2, line);
+				char *separator = " ";
+				char *arg = strtok(tempLine1, separator);
 				arg = strtok(NULL, separator);
-			}
-			char *args[argNum];
-			char *cmd = strtok(tempLine2, separator);
-			arg = strtok(NULL, separator);
-			int i = 0;
-			while(arg != NULL) {
-				args[i] = arg;
-				i++;
+				int argNum = 1;
+				while (arg != NULL) {
+					argNum++;
+					arg = strtok(NULL, separator);
+				}
+				char *args[argNum+1];
+				char *cmd = strtok(tempLine2, separator);
 				arg = strtok(NULL, separator);
+				args[0] = cmd;
+				int i = 1;
+				while(arg != NULL) {
+					args[i] = arg;
+					i++;
+					arg = strtok(NULL, separator);
+				}
+				args[argNum] = NULL;
+				i = 0;
+				while (i < argNum+1) {
+					printf("arg: %s\n", args[i]);
+					i++;
+				}
+				execvp(args[0], args);
+				fprintf(stderr, "Error: execvp failed\n");
+				exit(0);
 			}
-			execvp(cmd, args);
-			free(tempLine1);
-			free(tempLine2);
+			else {
+				fprintf(stderr, "Error: malloc tempLine2\n");
+				exit(0);
+			}
 		}
 		else {
-			fprintf(stderr, "Error: malloc tempLine2\n");
+			fprintf(stderr, "Error: malloc tempLine1\n");
 			exit(0);
 		}
 	}
+	
 	else {
-		fprintf(stderr, "Error: malloc tempLine1\n");
-		exit(0);
+		while (wait(&status) != pid){
+		}
+		
+		if (tempLine1 != NULL) {
+			free(tempLine1);
+		}
+		if (tempLine2 != NULL) {
+			free(tempLine2);
+		}
 	}
 }
 
 void executeCmd(char **commands, int numCmd) {
-	if (fork() == 0) {
-		int i;
-		for (i = 0; i < numCmd; i++) {
-			executeLine(commands[i]);
-		}
+	int i;
+	for (i = 0; i < numCmd; i++) {
+		executeLine(commands[i]);
 	}
 }
