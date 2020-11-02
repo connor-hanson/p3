984171b331a0675b5a6a2f382e99b481f02a0198
reatreify <70864815+reatreify@users.noreply.github.com>
2020-11-01 20:42:30 -0600

    Update graph.h

--- graph.h
+++ graph.h
@@ -19,9 +19,9 @@ GraphNode *addNode(char* name);
 //int removeNode(char* name);
 int addNodeDep(GraphNode *node, char *dep);
 int addNodeCmd(GraphNode *node, char *cmd);
-void executeNodes(GraphNode *root, char *visitedNodes);
+void executeNode(GraphNode *root, char *visitedNodes);
 GraphNode *getNode(GraphNode *startNode, char *name);
-char **getDependencies(char* dep); // what does this do again?
+//char **getDependencies(char* dep); // what does this do again?
 void freeNode(GraphNode *root);
 int getNumNodes();
 int childModded(GraphNode *parent, GraphNode *child);
